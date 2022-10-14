9ac47e8a8024f45d2c4454533ee978a59fa52501
TrostdorfA <sebatros@hotmail.com>
2022-10-14 14:47:03 -0300

    :sparkles: Feat: First task commit

---
+++ javascript/script.js
@@ -0,0 +1,45 @@
+// Checkpoint "Welcome to JavaScript"
+
+// String
+const name = "Andres";
+// String
+const lastName = "Trostdorf";
+// Number
+var age = 25;
+// String
+const dateOfBirth = "25/03/1997";
+// String
+var address = "Urdinarrain 258";
+// Boolean
+var isSingle = false;
+// String
+var favColor = "green";
+// Array of strings
+var favFood = ["pizza", "burger", "breaded", "pasta"];
+// String
+var favMovie = "Tron: Legacy";
+// Number
+const height = 1.75;
+// Object with properties
+var userFamily = {
+  father: "Martin",
+  mother: {
+    name: "Cristela",
+    lastName: "Mautone",
+    age: 51,
+    dateOfBirth: "26/11/1970",
+    favColors: ["pink", "green", "red"],
+  },
+  sister: "Milagros",
+  cousins: ["Gabriel", "Felipe", "Pablo", "Florencia"],
+  havePets: true,
+  petsData: {
+    name: "Paco",
+    age: 7,
+    type: "dog",
+    color: "white",
+    breed: "Caniche Toy",
+  },
+};
+
+console.log("userFamily", userFamily);
